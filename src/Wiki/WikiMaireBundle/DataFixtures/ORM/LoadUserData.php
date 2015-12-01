a9cd0d7979c6e45a7b6371de2bfabf7353bb2bb2
tmahamadou01 <mahamadoutraore1@gmail.com>
2015-12-01 14:20:28 +0100

    redirection des User et de l'admin

--- src/Wiki/WikiMaireBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Wiki/WikiMaireBundle/DataFixtures/ORM/LoadUserData.php
@@ -26,7 +26,7 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface
         // Get our userManager, you must implement `ContainerAwareInterface`
         $userManager = $this->container->get('fos_user.user_manager');
 
-        // Create our user and set details
+        // Creation de User et des details
         $user1 = $userManager->createUser();
         $user1->setUsername('admin');
         $user1->setPlainPassword('admin');
@@ -36,17 +36,17 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface
         $user1->setFirstname('Admin');
         $user1->setLastname('Admin');
 
-        // Create our user and set details
+        // Creation de User et des details
         $user2 = $userManager->createUser();
         $user2->setUsername('tra');
         $user2->setPlainPassword('tra');
         $user2->setEnabled(true);
         $user2->setEmail('admin2@email.com');
-        $user2->setRoles(array('ROLE_USER'));
+        $user2->setRoles(array('ROLE_ADMIN'));
         $user2->setFirstname('traore');
         $user2->setLastname('Mahamadou');
 
-        // Create our user and set details
+        // Creation de User et des details
         $user3 = $userManager->createUser();
         $user3->setUsername('bekele');
         $user3->setPlainPassword('bekele');
@@ -56,7 +56,7 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface
         $user3->setFirstname('bekele');
         $user3->setLastname('Mopara');
 
-        // Update the user
+        // Mise a jour des utilisateurs
         $userManager->updateUser($user1, true);
         $userManager->updateUser($user2, true);
         $userManager->updateUser($user3, true);
@@ -65,6 +65,6 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface
     }
     public function getOrder()
     {
-        return 1; // ordre d'appel
+        return 1; // call order
     }
 }
