55b5673d696174d52b6863c4d53da39c2aaf5621
SimonDanger <dangersimon76@gmail.com>
2015-12-01 19:01:10 +0100

    Ajout du mail de confirmation

--- src/Application/Sonata/UserBundle/Controller/ChangePasswordFOSUser1Controller.php
+++ src/Application/Sonata/UserBundle/Controller/ChangePasswordFOSUser1Controller.php
@@ -15,6 +15,8 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Wiki\WikiMaireBundle\Entity\Projet;
 
 /**
  * Class ChangePasswordFOSUser1Controller
@@ -39,4 +41,63 @@ class ChangePasswordFOSUser1Controller extends \Sonata\UserBundle\Controller\Cha
             return $this->container->get('router')->generate('sonata_user_profile_show');
     }
 
+    public function renderView($view, array $parameters = array())
+    {
+        return $this->container->get('templating')->render($view, $parameters);
+    }
+
+    public function get($id)
+    {
+        if ('request' === $id) {
+            @trigger_error('The "request" service is deprecated and will be removed in 3.0. Add a typehint for Symfony\\Component\\HttpFoundation\\Request to your controller parameters to retrieve the request instead.', E_USER_DEPRECATED);
+        }
+
+        return $this->container->get($id);
+    }
+
+    public function changePasswordAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $form = $this->container->get('fos_user.change_password.form');
+        $formHandler = $this->container->get('fos_user.change_password.form.handler');
+        
+        $process = $formHandler->process($user);
+        if ($process) {
+            $this->setFlash('fos_user_success', 'change_password.flash.success');
+
+            // On récupère le nouveau mot de passe
+            $newPassword = $user->getPlainPassword();
+
+            // On récupère l'adresse mail pour le mailing
+            $mailDestinataire = $user->getEmail();
+
+            // Envoie du mail
+            $sendMessage = \Swift_Message::newInstance()
+                ->setSubject('Modification de votre mot de passe')
+                ->setFrom('wikidesmaires@gmail.com')
+                ->setTo($mailDestinataire)
+                ->setBody(
+                    $this->renderView(
+                        'emails/receivedMessage.html.twig',
+                        array(
+                            'user' => $user,
+                            'password' => $newPassword
+                        )
+                    ),
+                    'text/html'
+                );
+            $this->get('mailer')->send($sendMessage);
+
+            return new RedirectResponse($this->getRedirectionUrl($user));
+        }
+
+        return $this->container->get('templating')->renderResponse(
+            'SonataUserBundle:ChangePassword:changePassword.html.'.$this->container->getParameter('fos_user.template.engine'),
+            array('form' => $form->createView())
+        );
+    }
 }
